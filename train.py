eaa14ea70a797bea4bfcc2b2128c42c22b4358c2
Lipunpun <david5291214@gmail.com>
2024-09-13 11:37:13 +0800

    0913更新數據集

--- train.py
+++ train.py
@@ -506,7 +506,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
 
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--weights", type=str, default=ROOT / "best.pt", help="initial weights path")
+    parser.add_argument("--weights", type=str, default=ROOT / "yolov5s.pt", help="initial weights path")
     parser.add_argument("--cfg", type=str, default="", help="model.yaml path")
     parser.add_argument("--data", type=str, default=ROOT / "data/onlyfloor.yaml", help="dataset.yaml path")
     parser.add_argument("--hyp", type=str, default=ROOT / "data/hyps/hyp.scratch-low.yaml", help="hyperparameters path")
